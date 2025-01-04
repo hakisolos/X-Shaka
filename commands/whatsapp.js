ec58c68d8d49eea57742c7cc2756a34f48f0d341
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-04 18:10:18 +0200

    Delete

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -10,11 +10,11 @@ CreatePlug({
         if(!ongroup) return;
         if(!owner) return;
         if (match.length < 2) return message.reply('usage: gc <welcome/goodbye> <on/off>');
-        const feature = args[0].toLowerCase() === 'welcome' ? 'on_welcome' : args[0].toLowerCase() === 'goodbye' ? 'on_goodbye' : null;
-        const state = args[1].toLowerCase() === 'on';
+        const feature = match[0].toLowerCase() === 'welcome' ? 'on_welcome' : args[0].toLowerCase() === 'goodbye' ? 'on_goodbye' : null;
+        const state = match[1].toLowerCase() === 'on';
         if (!feature) return;
         await toggle(message.user, feature, state);
-        message.reply(`${args[0].charAt(0).toUpperCase() + args[0].slice(1)} gc_events_now ${state ? 'enabled' : 'disabled'}`);
+        message.reply(`${match[0].charAt(0).toUpperCase() + match[0].slice(1)} gc_events_now ${state ? 'enabled' : 'disabled'}`);
     }
 });
           
