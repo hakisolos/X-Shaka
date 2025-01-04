af45b716ef1eead4e8f15fc9eec240fcf69b3b15
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-04 17:56:41 +0200

    whatsapp

---
+++ commands/whatsapp.js
@@ -0,0 +1,21 @@
+const { CreatePlug } = require('../lib/commands');
+const { toggle } = require('../database/group');
+
+CreatePlug({
+    command: 'gc',
+    category: 'group',
+    desc: 'Enable & disable',
+    execute: async (message, conn, match) => {
+        var ongroup = message.isGroup;
+        if(!ongroup) return;
+        var access = message.isBotAdmin;
+        if(!access) return message.reply('_um not an admin_');
+        if (match.length < 2) return message.reply('usage: gc <welcome/goodbye> <on/off>');
+        const feature = args[0].toLowerCase() === 'welcome' ? 'on_welcome' : args[0].toLowerCase() === 'goodbye' ? 'on_goodbye' : null;
+        const state = args[1].toLowerCase() === 'on';
+        if (!feature) return;
+        await toggle(message.user, feature, state);
+        message.reply(`${args[0].charAt(0).toUpperCase() + args[0].slice(1)} gc_events_now ${state ? 'enabled' : 'disabled'}`);
+    }
+});
+          
