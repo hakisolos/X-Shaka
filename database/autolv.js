47fd3f13de618a21f6bf668ebbaad623784b6792
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-06 20:38:38 +0200

    Delete

--- database/autolv.js
+++ database/autolv.js
@@ -1,38 +1,4 @@
-const db = require('quick.db');
-const categories = { 
-  naruto: ['Hokage', 'Akastuki Clan', 'Madara Uchiha', 'Uchiha', 'Akatsuki', 'Shinobi', 'Sannin', 'Rogue Ninja', 'Sasuke', 'Itachi', 'Naruto', 'Kurama 9tl'],
-  dragonBall: ['Son Goku', 'Vegita', 'Jiren', 'Blory', 'Gohan', 'Vegito', 'Gogeta', 'Gotenks', 'Namekian', 'Frieza Force', 'Z Fighter', 'Majin', 'Android', 'Beerus', 'Whis', 'Grand Zeno']
-};
-
-function cxl(level) {
-  if (level < 5) return categories.naruto[Math.floor(Math.random() * categories.naruto.length)];
-  if (level >= 5 && level < 10) return categories.dragonBall[Math.floor(Math.random() * categories.dragonBall.length)];
-  return categories.naruto[Math.floor(Math.random() * categories.naruto.length)];}
-async function maxUP(message, conn) {
-  const contact = message.user; 
-  const username = contact.split('@')[0] || 'astral';
-  let userXP = db.get(`${contact}.xp`) || 0;
-  let userLevel = db.get(`${contact}.level`) || 0;
-  userXP += 7;
-  const nextLevelXP = 5 * Math.pow(userLevel + 1, 2);
-  if (userXP >= nextLevelXP) {
-    userLevel += 1;
-    userXP -= nextLevelXP;
-    db.set(`${contact}.xp`, userXP);
-    db.set(`${contact}.level`, userLevel);
-    const img = await conn.profilePictureUrl(contact, 'image').catch(() => null);
-    const category = cxl(userLevel);
-    const maxSend = `🎉 **Level-Up** 🎉\n**Username**: ${username}\n**🎈 Level**: ${userLevel}\n**🎈 Category**: ${category}`;
-    if (img) {
-      await conn.sendMessage(contact, { image: { url: img }, caption: maxSend });
-    } else {
-      await conn.sendMessage(contact, { text: maxSend });
-    }
-  } else {
-    db.set(`${contact}.xp`, userXP);
-  }}
-
-async function detectACTION(update, conn) {
+async function announcementi(update, conn) {
   const { id, participants, action } = update;
   for (const participant of participants) {
     const zmg = await conn.profilePictureUrl(participant, 'image').catch(() => null);
@@ -52,5 +18,5 @@ async function detectACTION(update, conn) {
   }
 }
 
-module.exports = { maxUP, detectACTION };
+module.exports = { announcementi };
     
