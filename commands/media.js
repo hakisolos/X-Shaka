3747169ca0f112cce9aa5d7ace02f5b594a3b4f3
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-08 01:34:58 +0200

    Delete

--- commands/media.js
+++ commands/media.js
@@ -3,14 +3,18 @@ const { CreatePlug } = require('../lib/commands.js');
 CreatePlug({
     command: 'st2img',
     category: 'converter',
-    desc: 'Convert sticker to image.',
+    desc: 'Convert sticker to image',
     execute: async (message, conn, match) => {
-        if (!message.message || !message.message.stickerMessage) {
+        if (!message.message || !(message.message.stickerMessage || message.message.sticker)) {
         return message.reply('_Please send a sticker_');}
+        const args = match ? match.split(' ') : [];
+        const caption = args.length > 0 ? args.join(' ') : '*_Here is your image_*'; 
         const media = await conn.downloadMediaMessage(message);
         if (media) {
-        await conn.sendMessage(message.user, { image: media }, { caption: '*_Here is your image_*' });
-        } else {}
+            await conn.sendMessage(message.user, { image: media }, { caption });
+        } else {
+            return message.reply('_err_');
+        }
     }
 });
-              
+        
