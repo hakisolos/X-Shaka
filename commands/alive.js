78abea9f41559d45bda74d023d651cdb343148e2
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-08 15:24:41 +0200

    naxor

--- commands/alive.js
+++ commands/alive.js
@@ -1,17 +1,29 @@
 const { CreatePlug } = require('../lib/commands');
-const User = require('../database/alive');
+const CONFIG = require('../config');
 
 CreatePlug({
     command: 'alive',
     category: 'general',
-    desc: 'alive',
-    execute: async (message, conn) => {
-        //await message.react('🗣️');
-        const _user = await User.findOne({ where: { id: message.user } });
-        const msg = _user ? _user.generateAliveMessage() : '_not active_';   
-        await conn.sendMessage(message.user, { text: msg });
+    desc: 'alive msg',
+    execute: async (message, conn, match) => {
+        const Alive = require('../database/alive');
+        if (match?.startsWith('setalive')) {
+            const object = match.replace('setalive', '').trim();
+            const format = /^Time: @time\s+Date: @date\s+Runtime: @runtime\s+Message: +/;
+            if (!format.test(object)) {
+                await message.reply('_Please use the_format:\n\nTime: @time\nDate: @date\nRuntime: @runtime\nMessage: Your alive msg here_');
+                return;
+            }
+            await Alive.setAliveMessage(object);
+            await conn.sendMessage(message.user, { text: "Alive set successfully" });
+        } else {
+            const _msg = await Alive.getAliveMessage();
+            await conn.sendMessage(message.user, { text: _msg });
+        }
     }
 });
+
+
 
 CreatePlug({
     command: 'ping',
