1e212bfa42c80d69ee50e00a3a8c598b9dc676d7
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-12 00:13:16 +0200

    Delete

--- commands/group.js
+++ commands/group.js
@@ -4,9 +4,9 @@ CreatePlug({
     command: 'kick',
     category: 'group',
     desc: 'Kick members by country code or kick all members',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup || !message.isBotAdmin || !message.isAdmin) return;
-        const arg = match[1];
+        const arg = args[1];
         if (arg === 'all') {
             const { participants } = await conn.groupMetadata(message.user);
             const com = participants.filter(member => !member.admin && member.id !== conn.user.id);
@@ -31,7 +31,7 @@ CreatePlug({
     command: 'kickall',
     category: 'group',
     desc: 'kick all_',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         if(!message.isBotAdmin) return;
         if (!message.isAdmin) return;
@@ -50,7 +50,7 @@ CreatePlug({
     command: 'lockinvite',
     category: 'group',
     desc: 'Lock the group invite',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn) => {
         if (!message.isGroup) return;
         if(!message.isBotAdmin) return message.reply('_um not admin_');
         if (!message.isAdmin) return;
@@ -60,27 +60,10 @@ CreatePlug({
 });
 
 CreatePlug({
-    command: 'clearchat',
-    category: 'group',
-    desc: 'Clear all messages',
-    execute: async (message, conn, match) => {
-        if (!message.isGroup) return;
-        if (!message.isAdmin) return;
-        const data = await conn.groupMetadata(message.user);
-        const participants = data.participants;
-        const messages = await conn.loadMessages(message.user, 100);
-        for (const msg of messages) {
-            if (msg.key.fromMe) continue;
-            await conn.sendMessage(message.user, { delete: { remoteJid: message.user, fromMe: false, messageId: msg.key.id } });
-        }
-    },
-});
-
-CreatePlug({
     command: 'tagall',
     category: 'group',
     desc: 'taga users',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         var data = await conn.groupMetadata(message.user);
         var participants = data.participants.map(p => p.id.replace('@s.whatsapp.net', ''));
@@ -97,16 +80,16 @@ CreatePlug({
     command: 'promote',
     category: 'group',
     desc: 'Promote members',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         if (!message.isBotAdmin) return message.reply('_not an admin_');
         if (!message.isAdmin) return;
-        if (!match) return message.reply('_Please mention the user_');
+        if (!args) return message.reply('_Please mention the user_');
         let target; 
         if (message.message.extendedTextMessage && message.message.extendedTextMessage.contextInfo) {
             target = message.message.extendedTextMessage.contextInfo.mentionedJid[0];
         } else {
-            target = match.includes('@s.whatsapp.net') ? match : match + '@s.whatsapp.net';}
+            target = args.includes('@s.whatsapp.net') ? args : args + '@s.whatsapp.net';}
         if (!target) return;
         await conn.groupParticipantsUpdate(message.user, [target], 'promote');
         message.reply(`_promoted_ ${target.replace('@s.whatsapp.net', '')} as admin`);
@@ -117,16 +100,16 @@ CreatePlug({
     command: 'demote',
     category: 'group',
     desc: 'Demote members',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         if (!message.isBotAdmin) return message.reply('_um not an admin_');
         if (!message.isAdmin) return;
-        if (!match) return message.reply('_Please mention the user_');
+        if (!args) return message.reply('_Please mention the user_');
         let target;
         if (message.message.extendedTextMessage && message.message.extendedTextMessage.contextInfo) {
         target = message.message.extendedTextMessage.contextInfo.mentionedJid[0];
         } else {
-        target = match.includes('@s.whatsapp.net') ? match : match + '@s.whatsapp.net';}
+        target = args.includes('@s.whatsapp.net') ? args : args + '@s.whatsapp.net';}
         if (!target) return;
         await conn.groupParticipantsUpdate(message.user, [target], 'demote');
         message.reply(`_demoted_ ${target.replace('@s.whatsapp.net', '')}`);
@@ -137,7 +120,7 @@ CreatePlug({
     command: 'mute',
     category: 'group',
     desc: 'Mute the group',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         if (!message.isBotAdmin) return message.reply('_not_admin_');
         if (!message.isAdmin) return;
@@ -152,7 +135,7 @@ CreatePlug({
     command: 'unmute',
     category: 'group',
     desc: 'Unmute the group',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn) => {
         if (!message.isGroup) return;
         if (!message.isBotAdmin) return message.reply('_not an admin_');
         if (!message.isAdmin) return;
@@ -166,12 +149,12 @@ CreatePlug({
     command: 'remove',
     category: 'group',
     desc: 'Remove a member from the group',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         if (!message.isBotAdmin) return message.reply('_um not admin_');
         if (!message.isAdmin) return;
-        if (!match) return message.reply('_Please mention a member_');
-        let target = message.message.extendedTextMessage?.contextInfo?.mentionedJid[0] || (match.includes('@s.whatsapp.net') ? match : match + '@s.whatsapp.net');
+        if (!args) return message.reply('_Please mention a member_');
+        let target = message.message.extendedTextMessage?.contextInfo?.mentionedJid[0] || (args.includes('@s.whatsapp.net') ? args : args + '@s.whatsapp.net');
         if (!target) return;
         await conn.groupParticipantsUpdate(message.user, [target], 'remove')
             .then(() => message.reply(`_removed_ ${target.replace('@s.whatsapp.net', '')}`))
@@ -204,7 +187,7 @@ CreatePlug({
     command: 'invite',
     category: 'group',
     desc: 'group_invites',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn) => {
         const isAdmin = message.isAdmin;
         if (!isAdmin) return;
         var _invites  = await conn.groupInviteCode(message.user);
@@ -216,28 +199,12 @@ CreatePlug({
     command: 'setname',
     category: 'group',
     desc: 'Change the group name',
-    execute: async (message, conn, match) => {
+    execute: async (message, conn, args) => {
         if (!message.isGroup) return;
         if(!message.isBotAdmin) return;
         if (!message.isAdmin) return;
-        if (!match) return message.reply('_Please provide a name_');
-        await conn.groupUpdateSubject(message.user, match);
-        message.reply(`_Group name_: "${match}"`);
+        if (!args) return message.reply('_Please provide a name_');
+        await conn.groupUpdateSubject(message.user, args);
+        message.reply(`_Group name_: "${args}"`);
     },
 });
-
-CreatePlug({
-    command: 'gcpp',
-    category: 'group',
-    desc: 'Set a new group icon',
-    execute: async (message, conn, match) => {
-        if (!message.isGroup) return;
-        if(message.isBotAdmin); return;
-        if (!message.isAdmin) return;
-        if (!match) return message.reply('_Please send the image_');
-        const media = await conn.downloadMediaMessage(message);
-        await conn.updateProfilePicture(message.user, media);
-        message.reply('Group pp update');
-    },
-});
-                               
