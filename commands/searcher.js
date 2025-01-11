e02bc1a31cd531a25037351d8107aef225c0d73c
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-12 00:07:35 +0200

    Update searcher.js

--- commands/searcher.js
+++ commands/searcher.js
@@ -1,13 +1,13 @@
 const { CreatePlug } = require('../lib/commands');
-
+const { googleSearch } = require('../lib/scrappers');
+    
 CreatePlug({
     command: 'google',
     category: 'search',
     desc: 'Search on Google',
-    execute: async (message, conn, match) => {
-        const { googleSearch } = require('../lib/scrappers');
-        if (!match) return message.reply('_Please provide a query to search_');
-        const results = await googleSearch(match).catch(() => null);
+    execute: async (message, conn, args) => {
+        if (!args) return message.reply('_Please provide a query to search_');
+        const results = await googleSearch(args).catch(() => null);
         if (!results) return;
         if (results.length === 0) return message.reply('_oops_');
         let txt = '*Google Search Results:*\n\n';
