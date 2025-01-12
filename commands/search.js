146210f61d58a695be5ce3ebb760b251e4197923
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-12 12:35:39 +0200

    Delete

--- commands/search.js
+++ commands/search.js
@@ -5,9 +5,9 @@ CreatePlug({
     command: 'google',
     category: 'search',
     desc: 'Search on Google',
-    execute: async (message, conn, args) => {
-        if (!args) return message.reply('_Please provide a query to search_');
-        const results = await googleSearch(args).catch(() => null);
+    execute: async (message, conn, match) => {
+        if (!match) return message.reply('_Please provide a query to search_');
+        const results = await googleSearch(match).catch(() => null);
         if (!results) return;
         if (results.length === 0) return message.reply('_oops_');
         let txt = '*Google Search Results:*\n\n';
@@ -15,8 +15,7 @@ CreatePlug({
             txt += `*${index + 1}. ${result.title}*\n`;
             txt += `Link: ${result.link}\n`;
             if (result.description) {
-                txt += `Description: ${result.description}\n`;
-            }
+                txt += `Description: ${result.description}\n`;}
             txt += '\n';
         });
        await conn.sendMessage(message.user, { text: txt });
