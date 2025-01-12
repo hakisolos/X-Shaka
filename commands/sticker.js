c00c8051858b7b198dfceb6394fd8038dd873add
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-12 14:26:38 +0200

    Delete

--- commands/sticker.js
+++ commands/sticker.js
@@ -1,1 +1,17 @@
+const { CreatePlug } = require('../lib/commands');
+const { createSticker } = require('../lib/sticker'); 
+const CONFIG = require('../config'); 
 
+CreatePlug({
+  command: 'sticker',
+  category: 'converter',
+  desc: 'sticker maker img or vid',
+  execute: async (message, conn, match) => {
+    const pack = CONFIG.app.packname; 
+    if (!(message?.message?.imageMessage || message?.message?.videoMessage)) return message.reply('_Please *** image or video_');
+      const sti = await conn.downloadMediaMessage(message.message);
+      const baffu = await createSticker(sti, '512x512', 80, 0, packname);  
+      await conn.sendMessage(message.user, { sticker: baffu, packname: packname });
+  }
+});
+    
