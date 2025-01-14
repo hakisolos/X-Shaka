37e1db5f423b3e9987eb42ada82f2a30bffc2ec4
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-14 07:42:02 +0200

    Delete

--- commands/admin.js
+++ commands/admin.js
@@ -4,7 +4,8 @@ CreatePlug({
     command: 'block',
     category: 'admin',
     desc: 'Block a user',
-    execute: async (message, conn) => {
+    execute: async (message, conn, owner) => {
+        if(!owner) return;
         let target = message.quoted ? message.quoted.participant : message.mentions.length > 0 ? message.mentions[0] : message.sender;
         if (!target) return conn.sendMessage(message.user, { text: 'Please reply to a user' });
         await conn.updateBlockStatus(target, 'block');
@@ -12,3 +13,16 @@ CreatePlug({
     }
 });
           
+CreatePlug({
+    command: 'unblock',
+    category: 'admin',
+    desc: 'Unblock a user',
+    execute: async (message, conn, owner) => {
+        if(!owner) return;
+        let target = message.quoted ? message.quoted.participant : message.mentions.length > 0 ? message.mentions[0] : message.sender;
+        if (!target) return conn.sendMessage(message.user, { text: 'Please reply to a user' });
+        await conn.updateBlockStatus(target, 'unblock');
+        conn.sendMessage(message.user, { text: `_Unblocked_ @${target.split('@')[0]}`, mentions: [target] });
+    }
+});
+                                        
