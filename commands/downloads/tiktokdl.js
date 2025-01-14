0a7c26d1d2bdc2158d12e90b3a38ff3a0b8d6fa3
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-14 08:11:30 +0200

    Delete

---
+++ commands/downloads/tiktokdl.js
@@ -0,0 +1,29 @@
+var fetch = require('fetch');
+
+const tiktokdl = async (url) => {
+  const api = `https://diegoson-naxor-api.hf.space/tiktok?url=${url}`;
+  try {
+    const res = await fetch(api);
+    if (!res.ok) {
+      throw new Error('err');}
+    const data = await res.json();
+    if (data.status === 200) {
+      const videoData = data.data;
+      return {
+        title: videoData.title,
+        videoUrl: videoData.playUrl,
+        hdVideoUrl: videoData.hdPlayUrl,
+        musicTitle: videoData.musicTitle,
+        musicAuthor: videoData.musicAuthor,
+        playCount: videoData.playCount,
+        avatar: videoData.avatar,
+      };
+    } else {
+      throw new Error('Invalid');
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = tiktokdl;
