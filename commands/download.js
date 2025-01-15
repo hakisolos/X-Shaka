9371a8d1728278e9d0cc11ff13410d903aab588e
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-15 17:45:50 +0200

    Delete

--- commands/download.js
+++ commands/download.js
@@ -7,19 +7,14 @@ CreatePlug({
   desc: 'Download',
   execute: async (message, conn, match) => {
     if (!match) return message.reply('_Please provide app name_');
+    await message.react('🗣️');
     const search = `https://bk9.fun/search/apk?q=${match}`;
     const resi = await fetch(search).then((res) => res.json());
     if (!resi || !resi.BK9 || resi.BK9.length === 0) return;
     const down = `https://bk9.fun/download/apk?id=${resi.BK9[0].id}`;
     const mep = await fetch(down).then((res) => res.json());
     if (!mep || !mep.BK9 || !mep.BK9.dllink) return message.reply('_err');
-    const detail = {
-      document: { url: mep.BK9.dllink },
-      fileName: mep.BK9.name,
-      mimetype: "application/vnd.android.package-archive",
-      caption: `*${mep.BK9.name}*\nDownload your APK now`,
-    };
-
+    const detail = { document: { url: mep.BK9.dllink },fileName: mep.BK9.name, mimetype: "application/vnd.android.package-archive",caption: `*${mep.BK9.name}*\nDownloaded ur APK\n Made with❣️`,};
     await conn.sendMessage(message.user, detail, { quoted: message });
   },
 });
