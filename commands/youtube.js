d20ef12ecf0539c2897a883726bfdb7d6ba64179
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-16 17:21:59 +0200

    Delete

--- commands/youtube.js
+++ commands/youtube.js
@@ -1,4 +1,5 @@
 const { CreatePlug } = require('../lib/commands');
+const { Func } = require('./downloads/media');
 const YouTube = require('youtube-sr').default;
 const tiktokdl = require('./downloads/tiktokdl'); 
 
@@ -25,6 +26,55 @@ CreatePlug({
   execute: async (message, conn, match) => {
     if (!match) return message.reply('_Please provide a TikTok URL_');
     const videos = await tiktokdl(match).catch(error => message.reply(`${error}`));
-    await conn.sendMessage(message.user, { video: { url: videos.hdVideoUrl }, caption: `*Title:* ${videos.title}\n *Music*: ${videos.musicAuthor}`, }).catch(error => message.reply(`${error}`));
+    await conn.sendMessage(message.user, { video: { url: videos.hdVideoUrl }, caption: `*Title:* ${videos.title}\n *Music*: ${videos.musicAuthor}\nMade with❣️`, }).catch(error => message.reply(`${error}`));
   },
 });
+
+CreatePlug({
+  command: 'fb',
+  category: 'download',
+  desc: 'Download Facebook videos',
+  execute: async (message, conn, match) => {
+    if (!match) return message.reply('_Please provide a Facebook video URL_');
+    const result = await Func(match, 'facebook');
+    if (result.platform === 'facebook' && result.videoHD) {
+      await conn.sendMessage(message.user, {
+        video: { url: result.videoHD },
+        caption: `*Likes*: ${result.likes || 'ehe'}\n*Shares:* ${result.shares || 'N/A'}`,
+      });
+    } else {}
+  },
+});
+
+CreatePlug({
+  command: 'instagram',
+  category: 'download',
+  desc: 'Download Instagram videos',
+  execute: async (message, conn, match) => {
+    if (!match) return message.reply('_Please provide an Instagram video URL_');
+    const result = await Func(match, 'instagram');
+    if (result.platform === 'instagram' && result.mediaUrl) {
+      await conn.sendMessage(message.user, {
+        video: { url: result.mediaUrl },
+        caption: `*Likes:* ${result.likes || 'N/A'}\n*Shares:* ${result.shares || 'N/A'}`,
+      });
+    } else {}
+  },
+});
+
+CreatePlug({
+  command: 'tik',
+  category: 'download',
+  desc: 'Download TikTok videos',
+  execute: async (message, conn, match) => {
+    if (!match) return message.reply('_Please provide a TikTok video URL_');
+    const result = await Func(match, 'tiktok');
+    if (result.platform === 'tiktok' && result.content) {
+      await conn.sendMessage(message.user, {
+        video: { url: result.content },
+        caption: `*username:* ${result.username}\n*Likes:* ${result.stats.likes}\n*Shares:* ${result.stats.shares}`,
+      });
+    } else {}
+  },
+});
+        
