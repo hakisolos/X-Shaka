35bd0a4a3ceb2ef521703eaab4689d8e2bcd1840
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-18 07:33:36 +0200

    Delete

---
+++ commands/downloads/Play.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+/* @NaxorDev*/
+
+async function PlaySearch(query) {
+    const url = `https://api.rendigital.store/endepoin/playstore?query?${query}`;
+    try {
+        const voidi = await fetch(url);
+        const data = await voidi.json();
+        if (!data.status || !data.results || data.results.length === 0) {
+            return [];
+        }
+        return data.results.map(app => ({
+            name: app.nama,
+            developer: app.developer,
+            rating: app.rate2,
+            link: app.link,
+            developerPage: app.link_dev,
+            image: app.img,
+        }));
+    } catch (error) {
+        throw new Error(`${error.message}`);
+    }
+}
+
+module.exports = { PlaySearch };
