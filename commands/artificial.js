1aa49241145413cff348fe8b96933962ff0b5394
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-18 07:47:11 +0200

    Delete

--- commands/artificial.js
+++ commands/artificial.js
@@ -1,32 +1,14 @@
-const axios = require('axios');
 const { CreatePlug } = require('../lib/commands');
-
-const models = ["yanzgpt-revolution-25b-v3.5", "yanzgpt-legacy-72b-v3.5"];
-exports.YanzGPT = (query, prompt, model = models[0]) =>
-  axios("https://api.yanzgpt.my.id/v1/chat", {
-    method: "POST",
-    headers: {
-      authorization: "Bearer yzgpt-sc4tlKsMRdNMecNy",
-      "content-type": "application/json"
-    },
-    data: {
-      messages: [
-        { role: "system", content: prompt },
-        { role: "user", content: query }
-      ],
-      model
-    }
-  }).then(res => res.data);
+const { ChatGPT } = require('./downloads/gpt');
 
 CreatePlug({
-  command: 'gpt',
-  category: 'AI',
-  desc: 'ai',
+  command: 'ai',
+  category: 'Artficial',
+  desc: 'Ask ChatGPT anything',
   execute: async (message, conn, match) => {
-    if (!match) return message.reply('_need a prompt_');
-    exports.YanzGPT(match, "You are a helpful assistant", models[0])
-      .then(response => conn.sendMessage(message.user, { text: response, quoted: message }))
-      .catch(() => conn.sendMessage(message.user, { text: '_Error_', quoted: message }));
-  }
+    if (!match) return message.reply('_Please provide a prompt_');
+    await message.react('🗣️');
+    var voidi = await ChatGPT(match);
+    return message.reply(voidi);
+  },
 });
-  
