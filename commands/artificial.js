011c5fe4ab6b641229016aeab9c421d39ee68a72
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-18 21:19:35 +0200

    Delete

--- commands/artificial.js
+++ commands/artificial.js
@@ -1,5 +1,5 @@
 const { CreatePlug } = require('../lib/commands');
-const { ChatGPT, GeminiAI, SimAI } = require('./downloads/gpt');
+const { ChatGPT, GeminiAI } = require('./downloads/gpt');
 
 CreatePlug({
   command: 'gemini',
@@ -9,18 +9,6 @@ CreatePlug({
     await message.react('🗣️');
     if (!match) return message.reply('_Please provide a query_');
     var voidi = await GeminiAI(match);
-    return message.reply(voidi);
-  },
-});
-
-CreatePlug({
-  command: 'simai',
-  category: 'Artficial',
-  desc: 'Ask SimAI anything',
-  execute: async (message, conn, match) => {
-    await message.react('🗣️');
-    if (!match) return message.reply('_Please provide a query_');
-    var voidi = await SimAI(match);
     return message.reply(voidi);
   },
 });
@@ -36,3 +24,36 @@ CreatePlug({
     return message.reply(voidi);
   },
 });
+
+const axios = require('axios');
+const model = [
+    "yanzgpt-revolution-25b-v3.5", 
+    "yanzgpt-legacy-72b-v3.5" 
+];
+
+const GPT4o = (query) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await axios("https://api.yanzgpt.my.id/v1/chat", {
+                headers: {
+                    authorization: "Bearer yzgpt-sc4tlKsMRdNMecNy",
+                    "content-type": "application/json"
+                },
+                data: {
+                    messages: [
+                        {
+                            role: "user",
+                            content: query
+                        }
+                    ],
+                    model: "yanzgpt-revolution-25b-v3.5"
+                },
+                method: "POST"
+            });
+            resolve(response.data.choices[0].message.content);
+        } catch (error) {
+            reject(error.response ? error.response.data : error.message);
+        }
+    });
+};
+
