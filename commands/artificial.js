0e8b32cb3fc01559f67d7302193c10f8a0d4a88b
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-18 08:26:52 +0200

    Delete

--- commands/artificial.js
+++ commands/artificial.js
@@ -1,5 +1,29 @@
 const { CreatePlug } = require('../lib/commands');
-const { ChatGPT } = require('./downloads/gpt');
+const { ChatGPT, GeminiAI, SimAI } = require('./downloads/gpt');
+
+CreatePlug({
+  command: 'gemini',
+  category: 'Artficial',
+  desc: 'Ask Gemini AI anything',
+  execute: async (message, conn, match) => {
+    await message.react('🗣️');
+    if (!match) return message.reply('_Please provide a query_');
+    var voidi = await GeminiAI(match);
+    return message.reply(voidi);
+  },
+});
+
+CreatePlug({
+  command: 'simai',
+  category: 'Artficial',
+  desc: 'Ask SimAI anything',
+  execute: async (message, conn, match) => {
+    await message.react('🗣️');
+    if (!match) return message.reply('_Please provide a query_');
+    var voidi = await SimAI(match);
+    return message.reply(voidi);
+  },
+});
 
 CreatePlug({
   command: 'ai',
