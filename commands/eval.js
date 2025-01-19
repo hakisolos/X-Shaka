20ec189a7ef06f0c5acde64eae67b5d264cd4e24
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2025-01-19 09:28:41 +0200

    Delete

--- commands/eval.js
+++ commands/eval.js
@@ -5,15 +5,13 @@ const { CreatePlug } = require('../lib/commands');
 CreatePlug({
   command: 'eval',
   category: 'admin',
-  desc: 'JavaScript code (admin only)',
+  desc: 'Evaluate JavaScript code (admin only).',
   execute: async (message, conn, match) => {
     const owner = CONFIG.app.mods;
     if (!owner) return;
     if (!match) return message.reply('undefined');
-    let result;
-    let output;
-    result = await eval(`(async () => { ${match} })()`).catch(err => err.message);
-    output = typeof result !== 'string' ? util.inspect(result) : result;
-    await message.reply(`\`\`\`\n${output}\n\`\`\``);
+    const result = await eval(match);
+    const output = typeof result !== 'string' ? util.inspect(result) : result;
+    message.reply(`\`\`\`\n${output}\n\`\`\``);
   }
 });
