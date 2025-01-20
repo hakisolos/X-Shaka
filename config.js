29733e6043ebe860d4a7e096fb38a5502e04bad9
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-01-20 16:38:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : 
 const CONFIG = {
     app: {
         session_name: process.env.SESSION_NAME || '',
-        botname: process.env.BOTNAME || 'X-ASTRAL',
+        botname: process.env.BOTNAME || 'X-Shaka',
         version: require('./package.json').version,
-        packname: process.env.PACKNAME || '❤️NaxorDev',
+        packname: process.env.PACKNAME || 'haki-xer',
         env: process.env.NODE_ENV || 'development',
         prefix: process.env.COMMAND_PREFIX || '?',
         mode: toBool(process.env.MODE || "true"),
-        mods: process.env.MODS || '27686881509,27686567257',
-        me: process.env.ME || '27686881509,27686567257',
+        mods: process.env.MODS || '2349112171078',
+        me: process.env.ME || '2349112171078',
         sdb: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
     },
 };
